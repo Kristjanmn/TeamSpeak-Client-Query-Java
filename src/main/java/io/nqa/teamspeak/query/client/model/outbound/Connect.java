626bfcb9c2d44439db2b519f2a37bc036870a311
Kristjanmn <kristjanmn@hotmail.com>
2024-12-03 10:28:26 +0200

    Add Connect object

---
+++ src/main/java/io/nqa/teamspeak/query/client/model/outbound/Connect.java
@@ -0,0 +1,15 @@
+package io.nqa.teamspeak.query.client.model.outbound;
+
+import lombok.Builder;
+import lombok.ToString;
+
+@Builder
+@ToString
+public class Connect {
+    public String address;
+    public String password;
+    public String nickname;
+    public int channel;
+    public String channel_pw;
+    public String token;
+}
