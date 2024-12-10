c8bd6aab5b71cf51538a2772d027c9589db3df58
Kristjanmn <kristjanmn@hotmail.com>
2024-12-10 14:42:55 +0200

    Add MissingIdentifierException and TooManyIdentifiersException

---
+++ src/main/java/io/nqa/teamspeak/query/client/exception/MissingIdentifierException.java
@@ -0,0 +1,7 @@
+package io.nqa.teamspeak.query.client.exception;
+
+public class MissingIdentifierException extends TeamSpeakClientQueryException {
+    public MissingIdentifierException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/io/nqa/teamspeak/query/client/exception/TooManyIdentifiersException.java
@@ -0,0 +1,7 @@
+package io.nqa.teamspeak.query.client.exception;
+
+public class TooManyIdentifiersException extends TeamSpeakClientQueryException {
+    public TooManyIdentifiersException(String message) {
+        super(message);
+    }
+}
