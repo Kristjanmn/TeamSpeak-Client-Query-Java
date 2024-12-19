f41889033c5e250bd97deb4ba547f9fdddb358f8
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 18:51:27 +0200

    Update FileList, FileList.File and FileListFinished with documentation and constructors

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/FileList.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/FileList.java
@@ -1,23 +1,46 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnFileList;
 import lombok.ToString;
 
 import java.util.List;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnFileList} event.
+ */
 @ToString
 public class FileList extends TeamSpeakNotifyModel {
+    /** Channel ID */
     public Integer cid;
+    /** File browser path */
     public String path;
+    /** Return code */
     public String return_code;
+    /** List of files */
     public List<File> files;
 
+    /**
+     * Create empty FileList object.
+     */
+    public FileList() {}
+
+    /**
+     * Object containing File information.
+     */
     @ToString
     public static class File {
+        /** File name */
         public String name;
+        /** File size in bytes */
         public Long size;
+        /** File upload timestamp */
         public Long datetime;
+        /** File type */
         public Integer type;
+
+        /**
+         * Create empty File object.
+         */
+        public File() {}
     }
 }
--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/FileListFinished.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/FileListFinished.java
@@ -1,11 +1,20 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnFileListFinished;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnFileListFinished} event.
+ */
 @ToString
 public class FileListFinished extends TeamSpeakNotifyModel {
+    /** Channel ID */
     public Integer cid;
+    /** File browser path */
     public String path;
+
+    /**
+     * Create empty FileListFinished object.
+     */
+    public FileListFinished() {}
 }
