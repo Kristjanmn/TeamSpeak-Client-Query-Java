6d1d29abe08206dabaf60e8fa84b610825eee101
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 19:00:16 +0200

    Update ChannelMoved documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ChannelMoved.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ChannelMoved.java
@@ -1,17 +1,31 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.Reason;
+import io.nqa.teamspeak.query.client.event.OnChannelMoved;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnChannelMoved} event.
+ */
 @ToString
 public class ChannelMoved extends TeamSpeakNotifyModel {
     /** Channel ID */
     public Integer cid;
+    /** Parent channel ID */
     public Integer cpid;
+    /** Channel order */
     public Integer order;
+    /** Reason ID. See {@link Reason} */
     public Integer reasonid;
+    /** Invoker ID */
     public Integer invokerid;
+    /** Invoker name */
     public String invokername;
+    /** Invoker unique ID */
     public String invokeruid;
+
+    /**
+     * Create empty ChannelMoved object.
+     */
+    public ChannelMoved() {}
 }
