7304926366cb7661122f984a5b460e7049e397d6
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 18:47:46 +0200

    Update ClientChatClosed, ClientChatComposing with constructors and documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientChatClosed.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientChatClosed.java
@@ -1,13 +1,20 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnClientChatClosed;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnClientChatClosed} event.
+ */
 @ToString
 public class ClientChatClosed extends TeamSpeakNotifyModel {
     /** Client ID */
     public Integer clid;
     /** Client Unique ID */
     public String cluid;
+
+    /**
+     * Create empty ClientChatClosed object.
+     */
+    public ClientChatClosed() {}
 }
--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientChatComposing.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientChatComposing.java
@@ -1,13 +1,20 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnClientChatComposing;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnClientChatComposing} event.
+ */
 @ToString
 public class ClientChatComposing extends TeamSpeakNotifyModel {
     /** Client ID */
     public Integer clid;
     /** Client Unique ID */
     public String cluid;
+
+    /**
+     * Create empty ClientChatComposing object.
+     */
+    public ClientChatComposing() {}
 }
