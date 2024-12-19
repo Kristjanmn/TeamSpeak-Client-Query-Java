2076da020f0ca733b38a3998248bbf39ecd38852
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 19:16:21 +0200

    Update TeamSpeakNotifyModel with constructor

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/TeamSpeakNotifyModel.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/TeamSpeakNotifyModel.java
@@ -6,4 +6,9 @@ package io.nqa.teamspeak.query.client.model.inbound;
 public class TeamSpeakNotifyModel {
     /** Server connection handler ID */
     public Integer schandlerid;
+
+    /**
+     * Create empty TeamSpeakNotifyModel object.
+     */
+    public TeamSpeakNotifyModel() {}
 }
