4090ff551795a0c90f43a3b59947d80905fcf10d
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 18:48:52 +0200

    Update inbound ClientPoke with default constructor and documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientPoke.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientPoke.java
@@ -1,13 +1,24 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnClientPoke;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnClientPoke} event.
+ */
 @ToString
 public class ClientPoke extends TeamSpeakNotifyModel {
+    /** ID of the poking client */
     public Integer invokerid;
+    /** Name of the poking client */
     public String invokername;
+    /** Unique ID of the poking client */
     public String invokeruid;
+    /** Message poked with */
     public String msg;
+
+    /**
+     * Create empty ClientPoke object.
+     */
+    public ClientPoke() {}
 }
