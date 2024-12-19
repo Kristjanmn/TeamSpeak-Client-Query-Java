d832b77511263eea55296bf8cfadaa1e479f645c
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 19:08:49 +0200

    Update ChannelPasswordChanged documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ChannelPasswordChanged.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ChannelPasswordChanged.java
@@ -1,10 +1,18 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnChannelPasswordChanged;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnChannelPasswordChanged} event.
+ */
 @ToString
 public class ChannelPasswordChanged extends TeamSpeakNotifyModel {
+    /** Channel ID */
     public Integer cid;
+
+    /**
+     * Create empty ChannelPasswordChanged object.
+     */
+    public ChannelPasswordChanged() {}
 }
