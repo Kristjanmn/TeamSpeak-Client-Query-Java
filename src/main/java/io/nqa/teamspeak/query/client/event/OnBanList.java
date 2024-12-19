0501bd1f9f55c729c78c34711fc6d952f838ea22
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 18:58:34 +0200

    Update OnBanList event documentation

--- src/main/java/io/nqa/teamspeak/query/client/event/OnBanList.java
+++ src/main/java/io/nqa/teamspeak/query/client/event/OnBanList.java
@@ -4,10 +4,20 @@ import io.nqa.teamspeak.query.client.model.inbound.BanList;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
+/**
+ * Triggered upon receiving ban list from TeamSpeak client.<br>
+ * Provides {@link BanList} object.
+ */
 @Getter
 public class OnBanList extends ApplicationEvent {
+    /** List of bans received from client */
     private final BanList banList;
 
+    /**
+     * Cast the event.
+     *
+     * @param banList Object containing event details
+     */
     public OnBanList(BanList banList) {
         super(banList);
         this.banList = banList;
