555ad6d3ff585715b4c94ab4efa1a886487555c0
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 19:03:15 +0200

    Update StartDownload and StartUpload documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/StartDownload.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/StartDownload.java
@@ -1,15 +1,28 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnStartDownload;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnStartDownload} event.
+ */
 @ToString
 public class StartDownload extends TeamSpeakNotifyModel {
+    /** Client file-transfer ID */
     public Integer clientftfid;
+    /** Server file-transfer ID */
     public Integer serverftfid;
+    /** File-transfer key */
     public String ftkey;
+    /** Port */
     public Integer port;
+    /** File size */
     public Integer size;
+    /** File transfer protocol */
     public Integer proto;
+
+    /**
+     * Create empty StartUpload object.
+     */
+    public StartDownload() {}
 }
--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/StartUpload.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/StartUpload.java
@@ -1,15 +1,28 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnStartUpload;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnStartUpload} event.
+ */
 @ToString
 public class StartUpload extends TeamSpeakNotifyModel {
+    /** Client file-transfer ID */
     public Integer clientftfid;
+    /** Server file-transfer ID */
     public Integer serverftfid;
+    /** File-transfer key */
     public String ftkey;
+    /** Port */
     public Integer port;
+    /** Seek position */
     public Integer seekpos;
+    /** File transfer protocol */
     public Integer proto;
+
+    /**
+     * Create empty StartUpload object.
+     */
+    public StartUpload() {}
 }
