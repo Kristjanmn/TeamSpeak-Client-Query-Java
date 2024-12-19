058e40410331c57f088e47799e2111c31452429e
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 19:10:47 +0200

    Update ClientChannelGroupChanged documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientChannelGroupChanged.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientChannelGroupChanged.java
@@ -1,16 +1,30 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnClientChannelGroupChanged;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnClientChannelGroupChanged} event.
+ */
 @ToString
 public class ClientChannelGroupChanged extends TeamSpeakNotifyModel {
+    /** Invoker ID */
     public Integer invokerid;
+    /** Invoker name */
     public String invokername;
+    /** Invoker unique ID */
     public String invokeruid;
+    /** Channel group ID */
     public Integer cgid;
+    /** Channel group index */
     public Integer cgi;
+    /** Channel ID */
     public Integer cid;
+    /** Client ID */
     public Integer clid;
+
+    /**
+     * Create empty ClientChannelGroupChanged object.
+     */
+    public ClientChannelGroupChanged() {}
 }
