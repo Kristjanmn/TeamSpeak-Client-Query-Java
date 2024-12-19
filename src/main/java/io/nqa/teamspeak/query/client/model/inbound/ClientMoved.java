1226c0d7c7889daf9c19d29691fcb9606dc96501
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 19:11:47 +0200

    Update ClientMoved documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientMoved.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientMoved.java
@@ -1,18 +1,38 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.Reason;
+import io.nqa.teamspeak.query.client.event.OnClientMoved;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnClientMoved} event.
+ */
 @ToString
 public class ClientMoved extends TeamSpeakNotifyModel {
     /** Client ID */
     public Integer clid;
-    public Integer reasonid;           // 0 = switched | 1 - moved | 4 - kicked from channel
+    /**
+     * Reference {@link Reason}<br>
+     * 0 - switched<br>
+     * 1 - moved<br>
+     * 4 - kicked from channel
+     */
+    public Integer reasonid;
+    /** Channel from ID */
     public Integer cfid;
+    /** Channel to ID */
     public Integer ctid;
+    /** ID of the mover */
     public Integer invokerid;
+    /** Name of the mover */
     public String invokername;
+    /** Unique ID of the mover */
     public String invokeruid;
+    /** Reason for being moved */
     public String reasonmsg;
+
+    /**
+     * Create empty ClientMoved object.
+     */
+    public ClientMoved() {}
 }
