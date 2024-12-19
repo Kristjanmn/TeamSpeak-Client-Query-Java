e0085b8d68ed79379f34fccf5baaa6bb4a495e07
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 18:50:17 +0200

    Update ClientUpdated with default constructor, additional variables and documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientUpdated.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientUpdated.java
@@ -1,45 +1,98 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnClientUpdated;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnClientUpdated} event.
+ */
 @ToString
 public class ClientUpdated extends TeamSpeakNotifyModel {
     /** Client ID */
     public Integer clid;
+    /** Invoker ID */
+    public Integer invokerid;
+    /** Invoker name */
+    public String invokername;
+    /** Invoker unique ID */
+    public String invokeruid;
     /** Client version */
     public String client_version;
     /** Client platform */
     public String client_platform;
     /** Client nickname */
     public String client_nickname;
+    /** Client description */
+    public String client_description;
+    /** Client login name */
     public String client_login_name;
-    /** 1 = Client is away */
+    /** 1 - Client is away */
     public Integer client_away;
-    /** Timestamp of when client was created */
+    /** Away message */
+    public String client_away_message;
+    /** Timestamp of when client was created (first connected this server) */
     public Long client_created;
     /** Timestamp of when client was last connected */
     public Long client_lastconnected;
+    /** Total amount of client connections */
     public Integer client_totalconnections;
+    /** Number of bytes client has uploaded this month */
     public Long client_month_bytes_uploaded;
+    /** Number of bytes client has downloaded this month */
     public Long client_month_bytes_downloaded;
+    /** Number of total bytes client has uploaded */
     public Long client_total_bytes_uploaded;
+    /** Number of total bytes client has downloaded */
     public Long client_total_bytes_downloaded;
-    public Long client_input_hardware;
+    /** 1 - Client has input device */
+    public Integer client_input_hardware;
+    /** 1 - Client input is muted */
+    public Integer client_input_muted;
+    /** 1 - Client has output device */
+    public Integer client_output_hardware;
+    /** 1 - Client output is muted */
+    public Integer client_output_muted;
+    /** 1 - Client is priority speaker */
+    public Integer client_is_priority_speaker;
+    /** 1 - Client is channel commander */
+    public Integer client_is_channel_commander;
+    /** Number of unread messages */
+    public Integer client_unread_messages;
+    /** 1 - Client has avatar */
+    public Integer client_flag_avatar;
+    /** Signed badges */
+    public String client_signed_badges;
+    /** 1 - Client has requested permissions to talk */
+    public Integer client_talk_request;
+    /** 1 - Talk request message */
+    public String client_talk_request_msg;
+    /** 1 - Client is currently speaking */
+    public Integer client_is_talker;
+    /** 1 - Client is recording */
+    public Integer client_is_recording;
+    /** Client server groups. E.g. 532,821 */
+    public String client_servergroups;
+    /** Client MyTeamSpeak ID */
+    public String client_myteamspeakid;
+    /** Client badges */
+    public String client_badges;
+    /** Client MyTeamSpeak avatar */
+    public String client_myteamspeak_avatar;
+    /** Client i_client_talk_power */
+    public Integer client_talk_power;
+    /** Client icon ID */
+    public String client_icon_id;
+    /** Channel group inherited from channel? */
+    public Integer client_channel_group_inherited_channel_id;
+    /** Channel group ID */
+    public Integer client_channel_group_id;
+    /** Client i_client_needed_serverquery_view_power */
+    public Integer client_needed_serverquery_view_power;
+    /** Client tag */
+    public String client_user_tag;
 
-    // notifyclientupdated
-    // schandlerid=1
-    // clid=335
-    // client_version=3.6.2\s[Build:\s1695203293]
-    // client_platform=Windows
-    // client_login_name
-    // client_created=1396725449
-    // client_lastconnected=1728541859
-    // client_totalconnections=4762
-    // client_month_bytes_uploaded=1967516
-    // client_month_bytes_downloaded=83791033
-    // client_total_bytes_uploaded=6805212607
-    // client_total_bytes_downloaded=6335332969
-    // client_input_hardware=0
+    /**
+     * Create empty ClientUpdated object.
+     */
+    public ClientUpdated() {}
 }
