7832ec8bf93780c49ca05a6c22132ef667f37957
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 19:01:21 +0200

    Update TextMessage documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/TextMessage.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/TextMessage.java
@@ -1,29 +1,33 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnTextMessage;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnTextMessage} event.
+ */
 @ToString
 public class TextMessage extends TeamSpeakNotifyModel {
     /**
-     * 1 - Client<p>
-     * 2 - Channel<p>
-     * 3 - Server<p>
+     * 1 - Client<br>
+     * 2 - Channel<br>
+     * 3 - Server<br>
      */
     public Integer targetmode;
+    /** Message */
     public String msg;
+    /** Target client ID */
     public Integer target;     // target current client ID - ONLY when targetMode=1
+    /** Invoker ID */
     public Integer invokerid;  // invoker current client ID
+    /** Invoker name */
     public String invokername;
+    /** Invoker unique ID */
     public String invokeruid;
 
-    /* notifytextmessage
-     * schandlerid=1
-     * targetmode=1
-     * msg=would\slook\sdope\saf
-     * target=307
-     * invokerid=309
-     * invokername=ambi
-     * invokeruid=Jv\/d1+pX7\/q343RrIMPTTVpob+U= */
+
+    /**
+     * Create empty TextMessage object.
+     */
+    public TextMessage() {}
 }
