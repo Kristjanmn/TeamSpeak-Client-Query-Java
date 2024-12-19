8c4fe0ed238f3f3b6ca71ff332cf83e70ef2ba9b
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 19:12:02 +0200

    Update ClientNeededPermissions documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientNeededPermissions.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ClientNeededPermissions.java
@@ -1,13 +1,36 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnClientNeededPermissions;
 import lombok.ToString;
 
-@NoArgsConstructor
+import java.util.List;
+
+/**
+ * Provided with {@link OnClientNeededPermissions} event.
+ */
 @ToString
 public class ClientNeededPermissions extends TeamSpeakNotifyModel {
-    /** Permission ID */
-    public Integer permid;
-    /** Permission value */
-    public Integer permvalue;
+    /** List of permissions the client needs */
+    public List<Perm> perms;
+
+    /**
+     * Create empty ClientNeededPermissions object.
+     */
+    public ClientNeededPermissions() {}
+
+    /**
+     * Permission object
+     */
+    @ToString
+    public static class Perm {
+        /** Permission ID */
+        public Integer permid;
+        /** Permission value */
+        public Integer permvalue;
+
+        /**
+         * Create empty Perm object.
+         */
+        public Perm() {}
+    }
 }
