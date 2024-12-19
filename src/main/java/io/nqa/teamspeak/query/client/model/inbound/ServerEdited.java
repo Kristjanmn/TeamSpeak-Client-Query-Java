b4fff0466a22a13466d15e50c7a9eb75a009ed52
Kristjanmn <kristjanmn@hotmail.com>
2024-12-19 19:00:37 +0200

    Update ServerEdited documentation

--- src/main/java/io/nqa/teamspeak/query/client/model/inbound/ServerEdited.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/inbound/ServerEdited.java
@@ -1,13 +1,24 @@
 package io.nqa.teamspeak.query.client.model.inbound;
 
-import lombok.NoArgsConstructor;
+import io.nqa.teamspeak.query.client.event.OnServerEdited;
 import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ * Provided with {@link OnServerEdited} event.
+ */
 @ToString
 public class ServerEdited extends TeamSpeakNotifyModel {
+    /** Reason ID */
     public Integer reasonid;
+    /** Invoker ID */
     public Integer invokerid;
+    /** Invoker name */
     public String invokername;
+    /** Invoker unique ID */
     public String invokeruid;
+
+    /**
+     * Create empty ServerEdited object.
+     */
+    public ServerEdited() {}
 }
