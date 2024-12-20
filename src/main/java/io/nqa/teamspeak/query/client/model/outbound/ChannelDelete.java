fcdded4e9ba450ef1f80d0c8de92f030a0bd25bb
Kristjanmn <kristjanmn@hotmail.com>
2024-12-20 04:46:42 +0200

    Update ChannelDelete

--- src/main/java/io/nqa/teamspeak/query/client/model/outbound/ChannelDelete.java
+++ src/main/java/io/nqa/teamspeak/query/client/model/outbound/ChannelDelete.java
@@ -1,16 +1,55 @@
 package io.nqa.teamspeak.query.client.model.outbound;
 
-import lombok.Builder;
 import lombok.ToString;
 
 /**
  * Details on which channel to delete.
  */
-@Builder
 @ToString
 public class ChannelDelete {
     /** ID of channel to delete */
-    public Integer cid;
-    /** 1 = the channel will be deleted even if there are clients within */
+    public final Integer cid;
+    /** 1 - the channel will be deleted even if there are clients within */
     public Integer force;
+
+    /**
+     * Create empty ChannelDelete object.
+     *
+     * @param cid Channel ID
+     */
+    public ChannelDelete(int cid) {
+        this.cid = cid;
+    }
+
+    /**
+     * Create ChannelDeleted with parameters.
+     *
+     * @param cid channel ID
+     * @param force 1 - true
+     */
+    public ChannelDelete(int cid, int force) {
+        this.cid = cid;
+        this.force = force;
+    }
+
+    /**
+     * Set force to 1 (true)
+     *
+     * @return this object
+     */
+    public ChannelDelete force() {
+        this.force = 1;
+        return this;
+    }
+
+    /**
+     * Set force variable.
+     *
+     * @param force 1 - true
+     * @return this object
+     */
+    public ChannelDelete force(int force) {
+        this.force = force;
+        return this;
+    }
 }
